0da32d3401ecdd6b8f1af3a6226e17d3cd985607
Lenovo <Lenovo@DESKTOP-TCIFSAN>
2025-08-06 23:03:15 +0530

    Initial Commit

---
+++ c2tc/src/day23/v1/mvc/service/IEmployeeService.java
@@ -0,0 +1,16 @@
+package day23.v1.mvc.service;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import com.tnsif.daytwentythree.v1.mvc.Employee;
+
+public interface IEmployeeService {
+	
+	List<Employee> getAllEmployees() throws SQLException;
+	Employee getEmployeeById(int empId) throws SQLException;
+	void addEmployee(Employee e)throws SQLException;
+	boolean deleteEmployee(int empId) throws SQLException;
+	boolean updateEmployee(int empId, Employee e) throws SQLException;
+	
+}
